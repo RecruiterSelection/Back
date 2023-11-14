d877ac7aeef0c11aea1f7ed5da7f8891b59fe904
arthur-mendonca <arthu91@gmail.com>
2023-11-14 11:54:00 -0300

    user dto criado

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsString } from "@nestjs/class-validator";
+import { Role } from "@prisma/client";
+
+export class CreateUserDto {
+  @IsEmail()
+  email;
+  @IsString()
+  passwordHash;
+  @IsEnum(Role)
+  role;
+}
