ee7c9075a50e335a047d7d897e7bb03f17e371f6
arthur-mendonca <arthu91@gmail.com>
2023-11-14 09:07:06 -0300

    feat: tabelas users/recruiters/candidates criadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { JobsController } from './jobs/jobs.controller';
-import { UsersService } from './users/users.service';
-import { JobsService } from './jobs/jobs.service';
-import { PrismaService } from './prisma/prisma.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersController } from "./users/users.controller";
+import { UsersService } from "./users/users.service";
+import { JobsService } from "./jobs/jobs.service";
+import { PrismaService } from "./prisma/prisma.service";
+import { JobsController } from "./jobs/jobs.controller";
 
 @Module({
   imports: [],
