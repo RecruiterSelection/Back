d8ee113bf2142417b6b59f5d2cbe91cf6411a4f1
arthur-mendonca <arthu91@gmail.com>
2023-11-21 20:52:35 -0300

    update: login retorna userId

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,6 +18,7 @@ export class AuthService {
         const payload = { sub: user.id, email: user.email };
         return {
           accessToken: await this.jwtService.signAsync(payload),
+          userId: user.id,
         };
       }
     }
